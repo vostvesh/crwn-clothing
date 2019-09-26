7d0bd94a213e47ae23891e51d734eb8250f78e7f
Aliaksandr Shautsou <aliaksandr.shautsou@tele2.com>
2019-09-26 16:14:23 +0300

    added router and withRoute HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`menu-item ${size}`}>
-      <div 
+    <div 
+      className={`menu-item ${size}`} 
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`
         }}
       />
       <div className="content">
-        <h1 className="title">{ title.toUpperCase() }</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
